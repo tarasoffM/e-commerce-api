070496fc6790ef1a5e4bc0df33e82806388df572
Matt Tarasoff <tarasoff.matt@gmail.com>
2025-01-29 16:11:32 -0500

    setup basic database connection framework

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
--- queries.js
+++ queries.js
@@ -1,2 +1,12 @@
 // Description: This file is used to load the environment variables from the .env file.
 require(dotenv).config();
+
+// create database connection pool
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});

e1ca9c53469d1cf88a43859a24b468dc6beb57e7
Matt Tarasoff <tarasoff.matt@gmail.com>
2025-01-30 14:13:27 -0500

    minor edits to index.js

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const customerQueries = require('./queries/customer');
 const productQueries = require('./queries/products');
 const cartQueries = require('./queries/cart');
 const authQueries = require('./queries/auth');
+const orderQueries = require('./queries/order');
 db = {
     ...customerQueries,
     ...productQueries,
     ...cartQueries,
-    ...authQueries
+    ...authQueries,
+    ...orderQueries
 };
 
 // initialize body-parser to parse incoming parameters requests to req.body
@@ -27,6 +29,7 @@ app.use(
 app.get('/', (req, res) => {res.json({ info: 'Welcome to our store!' });});
 
 // routes for customer endpoints
+app.get('/customer', (req, res) => {db.getCustomers(req, res);});
 app.get('/customer/:id', (req, res) => {db.getCustomerById(req, res);});
 app.post('/customer', (req, res) => {db.newCustomer(req, res);});
 app.put('/customer/:id', (req, res) => {db.updateCustomer(req, res);});
@@ -34,7 +37,7 @@ app.delete('/customer/:id', (req, res) => {db.deleteCustomer(req, res);});
 
 // routes for product endpoints
 app.get('/products', (req, res) => {db.getProducts(req, res);});
-app.get('/product/:id', (req, res) => {db.getProductById(req, res);});
+app.get('/products/:id', (req, res) => {db.getProductById(req, res);});
 
 // routes for cart endpoints
 app.get('/cart/:id', (req, res) => {db.getCart(req, res);});
@@ -42,7 +45,7 @@ app.post('/cart', (req, res) => {db.addToCart(req, res);});
 app.delete('/cart/:id', (req, res) => {db.deleteFromCart(req, res);});
 
 // routes for order endpoints
-app.get('/orders', (req, res) => {db.getOrders(req, res);});
+app.get('/order', (req, res) => {db.getOrders(req, res);});
 app.get('/order/:id', (req, res) => {db.getOrderById(req, res);});
 app.post('/order', (req, res) => {db.addOrder(req, res);});
 

f829dcae8b093851a34024a3b16e043267cd9cd2
Matt Tarasoff <tarasoff.matt@gmail.com>
2025-01-30 23:57:16 -0500

    added passport and session with local username/password login

--- passport-config.js
+++ passport-config.js
@@ -3,15 +3,26 @@ const pool = require('./db.js');
 const bcrypt = require('bcrypt');
 
 const initialize = (passport) => {
-    authenticateCustomer = async (email, password, done) => {
+    const authenticateCustomer = async (email, password, done) => {
         try {
-            const user = await pool.query('SELECT * FROM customer WHERE email = $1', [email]);
-            if (user.rows.length === 0) {
+            const result = await pool.query('SELECT * FROM customer WHERE email = $1', [email]);
+            const user = result.rows[0];
+            console.log(user);
+            if (!user) {
+                console.log('1');
                 return done(null, false, { message: 'No user with that email' });
             }
-            if (await bcrypt.compare(password, user.rows[0].password)) {
+            console.log(password);
+            console.log(user.password);
+            const isMatch = await bcrypt.compare(password, user.password);
+            console.log('Password comparison:', isMatch);
+
+
+            if (isMatch) {
+                console.log('2');
                 return done(null, user);
             } else {
+                console.log('3');
                 return done(null, false, { message: 'Password incorrect' });
             }
         } catch(err) {
@@ -21,8 +32,18 @@ const initialize = (passport) => {
     };
 
     passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateCustomer));
-    passport.serializeUser((user, done) => {});
-    passport.deserializeUser((id, done) => {});
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+    passport.deserializeUser((id, done) => {
+        pool.query('SELECT * FROM customer WHERE id = $1', [id], (err, result) => {
+            if (err) {
+                console.log('Error deserializing user:', err);
+                return done(err);
+            }
+            return done(null, result.rows[0]);
+        });
+    });
 }
 
 module.exports = initialize;

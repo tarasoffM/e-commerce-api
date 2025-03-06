28ca0b4c91dd9acd8c21203c5d3d3472bb87a213
Matt Tarasoff <tarasoff.matt@gmail.com>
2025-03-06 11:02:49 -0500

    moved backend .env loction to backend folder for consistency

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: './.env' });
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
